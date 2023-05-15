60fc6d23b41ae6d838b903427db9ceb4ade1e875
Oliver Alex Kruse Schjelde <oliv327w@edu.zealand.dk>
2023-05-15 23:13:20 +0200

    Login funktion er hårdkodet ind. Ingen logik i forhold til at tage fra Database. Skal laves. username: admin password: password

--- Time-Plan.dk/Pages/LogIn/LogInPage.cshtml.cs
+++ Time-Plan.dk/Pages/LogIn/LogInPage.cshtml.cs
@@ -1,11 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using Time_Plan.dk.Data;
 
 public class LoginModel : PageModel
 {
+
+    private readonly Time_PlandkContext _context;
+
+    public LoginModel(Time_PlandkContext context)
+    {
+        _context = context;
+    }
+
+
+
+
     public void OnGet()
     {
     }
+
+    //public async Task<IActionResult> OnPostAsync(string lønNr, string password)
+    //{
+    //    var person = await _context.Person.FirstOrDefaultAsync(p => p.LønNr == lønNr);
+
+    //    if (person != null && person.Password == password)
+    //    {
+    //        // Successful login, redirect to the home page or another protected page
+    //        return RedirectToPage("/Index");
+    //    }
+    //    else
+    //    {
+    //        // Invalid credentials, display an error message
+    //        ModelState.AddModelError(string.Empty, "Invalid username or password.");
+    //        return Page();
+    //    }
+    //}
 
     public IActionResult OnPost(string username, string password)
     {
@@ -19,4 +49,8 @@ public class LoginModel : PageModel
             return Page();
         }
     }
+
+
+
+
 }
