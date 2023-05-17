ff5cfd2774841514fcfcd601887933479a0cc4ec
oliverkruse10 <kruse143@gmail.com>
2023-05-17 13:56:16 +0200

    Man kan nu logge ind. Og logge ud. Man er automatisk logget ud når siden lukkes. juhuu

--- Time-Plan.dk/Pages/Index.cshtml.cs
+++ Time-Plan.dk/Pages/Index.cshtml.cs
@@ -1,20 +1,76 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using Time_Plan.dk.Data;
 
-namespace Time_Plan.dk.Pages
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+
+    private readonly Time_PlandkContext _context;
+
+    public IndexModel(Time_PlandkContext context)
     {
-        private readonly ILogger<IndexModel> _logger;
+        _context = context;
+    }
 
-        public IndexModel(ILogger<IndexModel> logger)
+    [BindProperty]
+    public int LønNr { get; set; }
+
+    [BindProperty]
+    public string Password { get; set; }
+
+
+    public async Task<IActionResult> OnGet()
+    {
+        if (User.Identity.IsAuthenticated)
+            {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToPage("/LogOut/LogOutPage");
+            }
+
+
+        return Page();
+      
+    }
+
+
+
+
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        var user = _context.Person.FirstOrDefault(e => e.LønNr == LønNr && e.Password == Password);
+
+        if (user != null)
         {
-            _logger = logger;
+            var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, user.LønNr.ToString()),
+
+        };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var authProperties = new AuthenticationProperties
+            {
+                IsPersistent = false,
+            };
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+
+            return RedirectToPage("/AShift/Index");
         }
-
-        public void OnGet()
+        else
         {
-
+            ViewData["Message"] = "Forkert løn nummer eller password";
+            return Page();
         }
     }
+
+
+
+
 }
--- Time-Plan.dk/Program.cs
+++ Time-Plan.dk/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Time_Plan.dk.Data;
 var builder = WebApplication.CreateBuilder(args);
@@ -8,9 +9,33 @@ builder.Services.AddRazorPages();
 builder.Services.AddDbContext<Time_PlandkContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Time_PlandkContext") ?? throw new InvalidOperationException("Connection string 'Time_PlandkContext' not found.")));
 
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+{
+    options.LoginPath = "/LogIn/LogInPage";
+});
+
+builder.Services.AddRazorPages(options =>
+{
+    // Add authorization options
+    options.Conventions.AuthorizeFolder("/Admin/Index");
+    options.Conventions.AuthorizeFolder("/Admin/Create");
+    options.Conventions.AuthorizeFolder("/AShift/Oversigt");
+    options.Conventions.AuthorizeFolder("/Ashift/Create");
+
+});
 
 
 var app = builder.Build();
+
+
+
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -25,6 +50,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
